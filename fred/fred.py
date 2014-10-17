f42544b5d0a5af93a3c755cfb0a584a2cdfde4f6
Bob Clough <bob@clough.me>
2014-10-17 16:41:59 +0100

    Upfdated to 0.7 - support for alfredv2 board

--- fred/fred.py
+++ fred/fred.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import serial
-import pygame
 import sys
 import logging
 import socket
@@ -13,13 +12,9 @@ config = yaml.safe_load(config_f)
 config_f.close()
 
 mqttc = mosquitto.Mosquitto(config['door']['mqtt_name'])
-mqttc.connect("alfred", 1883, 60, True)
+mqttc.connect("acidburn", 1883, 60, True)
 
 ser = serial.Serial(config['serial']['port'], config['serial']['baud'], timeout=0.5)
-
-pygame.mixer.init(44100, -16, 2, 2048)
-pygame.mixer.music.set_volume(1)
-pygame.mixer.music.load("doorbell.ogg")
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -37,36 +32,34 @@ ch.setFormatter(formatter)
 
 logger.addHandler(ch)
 
-logging.info("FRED 0.5")
+logging.info("FRED 0.7")
 
 mqttc.publish("door/%s/rebooted" % config['door']['name'])
 
+ser.write('E')
+
 while 1:
-    mqttc.loop()
+    mqttc.loop(timeout=50)
     card_id = ser.readline().strip()
+
+    if card_id:
+        logging.debug(card_id)
 
     if not card_id:
 	pass
     elif card_id == 'B':
         if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.play()
             logging.info('Buzzer')
             mqttc.publish("door/%s/buzzer" % config['door']['name'])
-    elif card_id == 'O':
+    elif card_id == 'D0-0':
         logging.info('Door Button Pressed')
         mqttc.publish("door/%s/opened/button" % config['door']['name'])
         ser.write('1')
-    elif card_id == 'D':
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.play()
-            logging.info('Doorbell')
-            mqttc.publish("door/%s/doorbell" % config['door']['name'])
     elif (config['hardware']['version'] == 'old' and card_id != '00000000000000') or (card_id[0] == 'C'):
 	if config['hardware']['version'] == 'new':
 		card_id = card_id[1:]
         	if card_id[0:2] == '88':
                 	card_id = card_id[2:]
-
 
         logging.info("Card ID: %s", card_id)
         found = False
