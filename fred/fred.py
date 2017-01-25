78d661a8dd90641f40d37a5930e04114294aa3fb
Fahad Sadah <fahad@sadah.uk>
2017-01-25 16:47:27 +0000

    MQTT loop in separate thread, for automatic reconnection when network comes back online. QoS enabled

--- fred/fred.py
+++ fred/fred.py
@@ -35,11 +35,11 @@ logger.addHandler(ch)
 logging.info("FRED 0.7")
 
 mqttc.publish("door/%s/rebooted" % config['door']['name'])
+mqttc.loop_start()
 
 ser.write('E')
 
 while 1:
-    mqttc.loop()
     card_id = ser.readline().strip()
 
     if card_id:
@@ -49,11 +49,11 @@ while 1:
 	pass
     elif card_id == 'D0-0':
         logging.info('Door Button Pressed')
-        mqttc.publish("door/%s/opened/button" % config['door']['name'])
+        mqttc.publish("door/%s/opened/button" % config['door']['name'], qos=2)
         ser.write('1')
     elif card_id[0:3] == 'A1-' and int(card_id[3:7]) < 100:
         logging.info('Doorbell pressed')
-        mqttc.publish("door/%s/doorbell" % config['door']['name'])
+        mqttc.publish("door/%s/doorbell" % config['door']['name'], qos=1)
     elif (config['hardware']['version'] == 'old' and card_id != '00000000000000') or (card_id[0] == 'C'):
 	if config['hardware']['version'] == 'new':
 		card_id = card_id[1:]
@@ -73,14 +73,14 @@ while 1:
                 ser.write('G')
                 found = True
                 logging.info("%s found, %s opened the door!", card_id, member[1])
-                mqttc.publish("door/%s/opened/username" % config['door']['name'], member[1])
+                mqttc.publish("door/%s/opened/username" % config['door']['name'], member[1], qos=2)
         if not found:
             ser.write('R')
             logging.info("%s not found!", card_id)
-            mqttc.publish("door/%s/invalidcard" % config['door']['name'])
+            mqttc.publish("door/%s/invalidcard" % config['door']['name'], qos=2)
         members_f.close()
     elif (card_id[0] == 'A'):
-	mqttc.publish("sensor/door/%s/analog/%s" % (config['door']['name'], card_id[1]), card_id.split('-')[1])
+	mqttc.publish("sensor/door/%s/analog/%s" % (config['door']['name'], card_id[1]), card_id.split('-')[1], qos=2)
     elif (card_id[0] == 'D'):
-        mqttc.publish("sensor/door/%s/digital/%s" % (config['door']['name'], card_id[1]), card_id.split('-')[1])
+        mqttc.publish("sensor/door/%s/digital/%s" % (config['door']['name'], card_id[1]), card_id.split('-')[1], qos=2)
 
