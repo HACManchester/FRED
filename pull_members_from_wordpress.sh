490dc72ef1eab5a84e657bcda0b92962a4fcdee0
rossyst <43707667+rossyst@users.noreply.github.com>
2018-11-23 18:21:46 +0000

    Update pull_members_from_wordpress.sh

--- pull_members_from_wordpress.sh
+++ pull_members_from_wordpress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "${0%/*}"
 source "config.sh"
-curl https://members.hacman.org.uk/query2.php?key=${secretkey} -o "members" 
+curl https://members.hacman.org.uk/query2.php?key=${SECRETKEY} -o "members" 
